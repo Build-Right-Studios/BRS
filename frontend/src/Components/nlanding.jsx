020a7d94cde5d68270e1137475ff4ea4de499a96
Soumilmittal <mittasoumill@gmail.com>
2025-07-21 23:44:41 +0530

    made footer

--- frontend/src/Components/nlanding.jsx
+++ frontend/src/Components/nlanding.jsx
@@ -401,11 +401,6 @@ export default function LandingPage() {
           ></motion.span>
         </motion.button>
       </section>
-
-      {/* Footer */}
-      <footer className="relative z-10 bg-[#583d2b] text-[#eee3cb] py-8 text-center border-t border-[#583d2b]/30">
-        <p>&copy; {new Date().getFullYear()} Build Right Studios. All rights reserved.</p>
-      </footer>
     </div>
   );
 }
