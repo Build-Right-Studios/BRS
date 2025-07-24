8adf72854822e80af1f625d87916af41cb02e64b
Soumilmittal <mittasoumill@gmail.com>
2025-07-24 10:44:23 +0530

    location chnage and name corrected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import ComingSoon from './Components/landing.jsx'
 import Nav from './Components/nav.jsx'
 import LandingPage from './Components/nlanding.jsx'
-import Footer from './footer.jsx';
+import Footer from './Components/footer.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
